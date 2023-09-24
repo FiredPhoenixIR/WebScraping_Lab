fe5781c4f3a29531a896266299951f803441c285
Mahdi ST <84761387+FiredPhoenixIR@users.noreply.github.com>
2023-09-24 21:55:43 +0330

    Update Scraping_3.py

--- Scraping_3.py
+++ Scraping_3.py
@@ -51,3 +51,27 @@ read_html_pandas_data = pd.read_html(url) #returns a list of all the tables foun
 read_html_pandas_data = pd.read_html(str(soup)) #convert the BeautifulSoup object to a string
 netflix_dataframe = read_html_pandas_data[0]
 '''netflix_dataframe.head()'''
+########### TEST #############
+practice_url="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/amazon_data_webpage.html"
+practice_data = requests.get(practice_url).text
+soup2 = BeautifulSoup(practice_data,'html5lib')
+'''soup.title'''
+amazon_data = pd.DataFrame(columns=["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"])
+tbody = soup2.find("tbody")
+# Using beautiful soup extract the table with historical share prices and store it into a dataframe named amazon_data
+for rows in tbody.find_all("tr"):
+    col = rows.find_all("td")
+    date=col[0].text
+    Open=col[1].text
+    high=col[2].text
+    low=col[3].text
+    close=col[4].text
+    adj_close=col[5].text
+    volume=col[6].text
+    amazon_data = amazon_data.append({"Date":date, "Open":Open, "High":high, "Low":low, "Close":close, "Adj Close":adj_close, "Volume":volume}, ignore_index=True)
+
+'''
+amazon_data.columns
+amazon_data.tail(1)
+#amazon_data.iloc[69]
+'''
